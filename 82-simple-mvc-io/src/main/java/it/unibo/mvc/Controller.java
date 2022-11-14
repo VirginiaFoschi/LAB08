0c8b331e77cb338a9698249e352f9cd83138ee9b
Virginia Foschi <virginia.foschi@studio.unibo.it>
2022-11-14 21:27:53 +0100

    implemented class Controller.java

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.io.*;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private File f;
+    private static final String SEP = File.separator;
+    public static final String FILE_NAME = System.getProperty("user.home")+ SEP+"output.txt";
+
+    public Controller() {
+        this.f = new File(Controller.FILE_NAME);
+    }
+
+    public Controller(final File f) {
+        this.f= f;
+    }
+
+    public File getFile() {
+        return this.f;
+    }
+
+    public String getPath() {
+        return this.f.getPath();
+    }
+
+    public void saveString(final String s) throws IOException {
+        try(final BufferedWriter w=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f)))){
+            w.write(s);
+        }catch(IOException e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
 
 }
