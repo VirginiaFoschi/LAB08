c5f333ab2b4260a9caa5783aaecd45da9efebacd
Virginia Foschi <virginia.foschi@studio.unibo.it>
2022-11-15 17:44:07 +0100

    implemented interface Controller.java

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    public void setString(String s);
+
+    public String getString ();
+
+    public List<String> getHistory();
+
+    public void printString() throws IllegalStateException;
 
 }
